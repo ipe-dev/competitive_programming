9f2089895682f25dbe2f255324c51fe551ea4d74
ipe <izaki.yb1998@gmail.com>
2024-09-01 06:49:41 +0900

    別解追記

--- abc/ABC088B.py
+++ abc/ABC088B.py
@@ -9,3 +9,12 @@ for i in range(1, n + 1):
     if i % 2 == 0:
         bob += cards.pop(cards.index(max(cards)))
 print(alice - bob)
+
+# 別解 こっちの方が処理早い
+# cards.sort(key=None, reverse=True)
+# for i in range(0, n):
+#     if i % 2 == 0:
+#         alice += cards[i]
+#     else:
+#         bob += cards[i]
+# print(alice - bob)
